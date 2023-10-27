a8940a03f2beb2f7067b343cc00dbcd93f74c92e
elegroag <maxedwwin@gmail.com>
2023-10-26 20:24:15 -0500

    feat: :fire: comserva@v1-001
    actualizacion core codeigniter version 4.4.3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,11 @@ $routes = Services::routes();
  * Router Setup
  * --------------------------------------------------------------------
  */
+if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+{
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
 $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
@@ -20,7 +25,7 @@ $routes->set404Override();
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
 // Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-// $routes->setAutoRoute(false);
+$routes->setAutoRoute(false);
 
 
 /*
